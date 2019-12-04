3c48c175bf318190eb4fb96fdf7f639cec949520
yuansiliang <yuansiliang@gmail.com>
2019-12-04 09:37:35 +0800

    init

---
+++ chaos_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chaos_ios
+//
+//  Created by  袁斯亮 on 2019/12/4.
+//  Copyright © 2019  袁斯亮. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
